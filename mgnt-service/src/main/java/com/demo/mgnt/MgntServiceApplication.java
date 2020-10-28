0e48f72c1273ce8a6c5bd9b179d1fad6c43e9fd4
ts <1045491420@qq.com>
2020-10-28 15:14:28 +0800

    整合activemq

--- mgnt-service/src/main/java/com/demo/mgnt/MgntServiceApplication.java
+++ mgnt-service/src/main/java/com/demo/mgnt/MgntServiceApplication.java
@@ -3,9 +3,11 @@ package com.demo.mgnt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @ServletComponentScan({"com.demo.mgnt.config.filter"})
+@EnableJms
 public class MgntServiceApplication {
 
     public static void main(String[] args) {
---
+++ mgnt-service/src/main/java/com/demo/mgnt/config/config/ActiveMqConfig.java
@@ -0,0 +1,82 @@
+package com.demo.mgnt.config.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
+
+import javax.jms.*;
+
+@Configuration
+public class ActiveMqConfig {
+
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
+    @Value("${spring.activemq.user}")
+    private String username;
+
+    @Value("${spring.activemq.password}")
+    private String password;
+
+    @Bean
+    public ConnectionFactory connectionFactory(){
+        return new ActiveMQConnectionFactory(username,password,brokerUrl);
+    }
+
+    // 在Queue模式中，对消息的监听需要对containerFactory进行配置
+    @Bean("queueListener")
+    public JmsListenerContainerFactory<?> queueJmsListenerContainerFactory(ConnectionFactory connectionFactory){
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setPubSubDomain(false);
+        return factory;
+    }
+
+    //在Topic模式中，对消息的监听需要对containerFactory进行配置
+    @Bean("topicListener")
+    public JmsListenerContainerFactory<?> topicJmsListenerContainerFactory(ConnectionFactory connectionFactory){
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setPubSubDomain(true);
+        return factory;
+    }
+
+    //queue模式的消费者
+    //@JmsListener(destination = "spring-queue",containerFactory = "queueListener")
+    public void showQueue(Message message) throws JMSException {
+        System.out.println("queue接受到----->"+((TextMessage)message).getText());
+    }
+
+    //topic模式的消费者
+    //@JmsListener(destination="spring-topic", containerFactory="topicListener")
+    public void readActiveQueue(Message message) throws JMSException {
+        System.out.println("topic接受到----->"+((TextMessage)message).getText());
+    }
+
+    public String getBrokerUrl() {
+        return brokerUrl;
+    }
+
+    public void setBrokerUrl(String brokerUrl) {
+        this.brokerUrl = brokerUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
